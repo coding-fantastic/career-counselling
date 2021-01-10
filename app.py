d1f6f52b419036a493c05a5c0d60c674ae6d8f0e
coding-fantastic <alexnzioka@protonmail.com>
2021-01-11 00:56:23 +0300

    update

--- app.py
+++ app.py
@@ -896,15 +896,15 @@ class PersonalityTest(KnowledgeEngine):
         'careersToAvoid':{
             "Artist":"Although ISTP types like to create tangible things in their work, they prefer practical work to creative pursuits.", 
             
-            Clergy: As with social work, this career path is not a good fit for the ISTP type’s preference for logical, analytical tasks.
+            "Clergy": "As with social work, this career path is not a good fit for the ISTP type’s preference for logical, analytical tasks.",
 
-            Manager: ISTP types prefer to work on detailed tasks rather than deal with coworker dynamics and big-picture projects.
+            "Manager": "ISTP types prefer to work on detailed tasks rather than deal with coworker dynamics and big-picture projects.",
 
-            Marketer: There are no clear-cut solutions to marketing challenges, which can be frustrating for problem-solving ISTP types.
+            "Marketer": "There are no clear-cut solutions to marketing challenges, which can be frustrating for problem-solving ISTP types.",
 
-            Receptionist: The social requirements of this job are usually overwhelming for an introverted ISTP type.
+            "Receptionist": "The social requirements of this job are usually overwhelming for an introverted ISTP type.",
 
-            Social worker: Working with vulnerable populations can be emotionally taxing, especially for ISTP types.
+            "Social worker": "Working with vulnerable populations can be emotionally taxing, especially for ISTP types."
         }
         }))
 
